11e9723d9a149eb92aadb8cce4302293c84e38af
nickblaq <nickookoro200@gmail.com>
2023-07-21 23:53:53 +0100

    jar 0.1

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -7,7 +7,7 @@ export default function Navbar () {
         <>
         <div className=' transition translate-y-6 ease-in-out duration-150 pb-6 h-16 md:py-0'>
             <nav className="flex h-full items-center justify-between max-w-sm px-6 mx-auto border border-[#bbb] rounded-xl">
-                <h1 className='text-2xl font-bold'>Waste Jar</h1>
+                <h1 className='text-2xl font-bold animate-ping'>500 naira / 2 bags only</h1>
                 <Trash  className='w-7 h-7 ml-1 animate-[wiggle_1s_ease-in-out_infinite]'/>
             </nav>
         </div>
